5fcc212c2a60b5912b2ca60d8b4d196645b5a95d
Qing Zhou <13247125437@163.com>
2025-08-03 11:24:53 +0800

    Add files via upload

---
+++ src/PM.java
@@ -0,0 +1,28 @@
+package vmpproblem;
+
+public class PM {
+	public int id;
+	public int cpuCap;
+	public int ramCap;
+	public int cpuUsage;
+	public int ramUsage;
+	public int vmNumber;
+	public boolean isOverloaded;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public PM(int id,int cpuCap,int ramCap,int cpuUsage,int ramUsage, int vmNumber, boolean isOverloaded){
+		this.id = id;
+		this.cpuCap = cpuCap;
+		this.ramCap = ramCap;
+		this.cpuUsage = cpuUsage;
+		this.ramUsage = ramUsage;
+		this.vmNumber = vmNumber;
+		this.isOverloaded = isOverloaded;
+	}
+	
+}
